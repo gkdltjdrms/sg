937b8179376ee7ffafbc2549a7015a453e81fa78
gkdltjdrms <gkdltjdrms@naver.com>
2023-01-30 14:39:35 +0900

    0130

--- study/keras/keras52_ensamble3.py
+++ study/keras/keras52_ensamble3.py
@@ -8,15 +8,17 @@ x1_datasets = np.array([range(100), range(301,401)]).transpose()
 x2_datasets = np.array([range(101,201), range(411,511),range(150,250)]).T
 # print (x2_datasets.shape) # (100, 3)
 x3_datasets = np.array([range(100,200),range(1301,1401)]).T
-y = np.array(range(2001,2101)) # 삼성
+y1 = np.array(range(2001,2101)) # 삼성
 y2 = np.array(range(201,301))  # 아모레
-x1_train, x1_test, x2_train, x2_test,x3_train,x3_test,y_train, y_test,y2_train,y2_test =train_test_split(x1_datasets,
-                                                                       x2_datasets,x3_datasets,
-                                                                       y,y2,train_size=0.7,
-                                                                       random_state=1234)
+x1_train, x1_test, x2_train, x2_test,x3_train,\
+x3_test,y1_train, y1_test,y2_train,y2_test\
+        =train_test_split(x1_datasets,
+        x2_datasets,x3_datasets,
+        y1,y2,train_size=0.7,
+        random_state=1234)
 
-print(x1_train.shape,x2_train.shape,x3_train.shape,y2_train.shape)
-
+print(x1_train.shape,x2_train.shape,x3_train.shape,y1_train.shape,y2_train.shape)
+# (70, 2) (70, 3) (70, 2) (70,) (70,)
 #2-1 모델
 input1 = Input(shape=(2,))
 dense1 = Dense(11, activation='relu', name='ds11')(input1)
@@ -46,15 +48,25 @@ model = Model(inputs=[input1, input2, input3], outputs=last_output)
 model.summary()
 
 
+#2-5 모델
 
+dense51 = Dense(21, activation='relu', name='ds51')(last_output)
+dense52 = Dense(22, activation='relu', name='ds52')(dense51)
+output5 = Dense(1, activation='relu', name='ds53')(dense52)
 
+#2-6 모델 분기
+dense61 = Dense(21, activation='relu', name='ds61')(last_output)
+dense62 = Dense(22, activation='relu', name='ds62')(dense61)
+output6 = Dense(1, activation='relu', name='ds63')(dense62)
+
+model = Model(inputs=[input1,input2,input3],outputs=[output5,output6])
 
 #3 컴파일, 훈련
 model.compile(loss = 'mse',optimizer='adam')
-model.fit([x1_train,x2_train,x3_train],y_train, epochs=500,batch_size=32)
+model.fit([x1_train,x2_train,x3_train],[y1_train,y2_train], epochs=500,batch_size=32)
 
 #4 평가, 예측
-loss = model.evaluate([x1_test, x2_test, x3_test], y_test)
+loss = model.evaluate([x1_test, x2_test, x3_test], [y1_test,y2_test])
 print('Loss:', loss)
 
 y_pred = model.predict([x1_test, x2_test, x3_test])
