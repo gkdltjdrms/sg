4fab8390b7a3c4b1e534e651f7a52ab5e3b823a2
gkdltjdrms <121652277+gkdltjdrms@users.noreply.github.com>
2023-02-06 09:40:32 +0900

    Create For03.java

---
+++ study java/03_loop/For03.java
@@ -0,0 +1,20 @@
+package for_;
+
+public class For03 {
+
+	public static void main(String[] args) {
+		int i, sum=0,mul=1;
+		for(i=1; i<=10; i++ ) {//1 2 3 4~~10
+		
+		sum = sum +=i; //1
+		mul = mul * i;
+		System.out.println(i+ "\t"+sum+"\t"+mul);
+		
+	
+		
+	
+		}
+	
+	}
+
+}
