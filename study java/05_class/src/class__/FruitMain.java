91c9f33ac6def343dca0869f9908d2493f6da3dc
gkdltjdrms <121652277+gkdltjdrms@users.noreply.github.com>
2023-02-14 18:26:16 +0900

    Add files via upload

--- study java/05_class/src/class__/FruitMain.java
+++ study java/05_class/src/class__/FruitMain.java
@@ -1,18 +1,73 @@
 package class__;
 
+import java.util.Scanner;
+
 class Fruit {
+	private String pum;
+    private int jan, feb, mar, tot;
+    private static int sumJan;
+    private static int sumFeb;
+    private static int sumMar;
 	
-}
+	
+	
+	public Fruit(String pum, int jan, int feb, int mar) {
+		this.pum=pum;
+		this.jan=jan;
+		this.feb=feb;
+		this.mar=mar;
+		this.calc();
+	}
+	
+	
+	
+	
+	public void calc() {
+		this.tot = this.jan + this.feb + this.mar;
+		  Fruit.sumJan += this.jan;
+	      Fruit.sumFeb += this.feb;
+	      Fruit.sumMar += this.mar;
+		
+	}
+	   public void display() {
+	        System.out.printf("%-8s%5d%5d%5d%7d\n", this.pum, this.jan, this.feb, this.mar, this.tot);
+	    }
+	
+	   public static void output(Fruit[] fruits) {
+		   int sumjan =0;
+		   int sumfeb =0;
+		   int summar =0;
+	        System.out.println("---------------------------------");
+	        System.out.println("PUM      JAN   FEB   MAR      TOT");
+	        System.out.println("---------------------------------");
+	        for (Fruit fruit : fruits) {
+	            fruit.display();
+	            sumjan += fruit.jan;
+	            sumfeb += fruit.feb;
+	            summar += fruit.mar;
+	        }
+	        System.out.println("---------------------------------");
+	        System.out.printf("        %3d   %3d   %3d\n", Fruit.sumJan, Fruit.sumFeb, Fruit.sumMar);
+	    }
+		
+		
+	}
+	
+	
+	
+
 //------------------
 public class FruitMain {
 
 	public static void main(String[] args) {
-		
-		
-		
+		 Fruit[] fruits = {
+	                new Fruit("사과", 100, 80, 75),
+	                new Fruit("포도", 30, 25, 10),
+	                new Fruit("딸기", 25, 30, 90)
+	        };
+	        Fruit.output(fruits);
+	    }
 	}
-
-}
 
 
 /*
