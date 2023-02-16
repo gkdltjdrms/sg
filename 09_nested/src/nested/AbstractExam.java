4778b6acfd7e75b5ea3cd04388eacd2dcd093a68
gkdltjdrms <121652277+gkdltjdrms@users.noreply.github.com>
2023-02-16 17:24:59 +0900

    Delete 09_nested directory

--- 09_nested/src/nested/AbstractExam.java
+++
@@ -1,7 +0,0 @@
-package nested;
-
-public abstract class AbstractExam {
-	public void cc() {}//empty body
-	public void dd() {}
-	
-}
--- 09_nested/src/nested/InterA.java
+++
@@ -1,6 +0,0 @@
-package nested;
-
-public interface InterA {
-	public abstract void aa();
-	public void bb();
-}
