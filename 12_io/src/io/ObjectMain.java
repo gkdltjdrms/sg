ff69f249d12e6534c3ea99a0706cb3beda80440d
gkdltjdrms <121652277+gkdltjdrms@users.noreply.github.com>
2023-02-17 15:03:27 +0900

    Add files via upload

---
+++ 12_io/src/io/ObjectMain.java
@@ -0,0 +1,38 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class ObjectMain {
+
+	public static void main(String[] args) throws  IOException, ClassNotFoundException {
+		PersonDTO dto = new PersonDTO("홍길동", 25, 185.3);
+		
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("result2.txt"));
+		oos.writeObject(dto);
+		oos.close();
+		
+		//파일의 내용을 읽어오기
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("result2.txt"));
+		 PersonDTO dto2;
+		
+			dto2 = (PersonDTO) ois.readObject();
+			  System.out.println("이름 = " + dto2.getName() + " 나이 = " + dto2.getAge() + " 키 = " + dto2.getHeight());
+
+			     ois.close();
+	
+
+	   
+		
+	}
+
+}
+
+/*
+ * try catch , class notfound
+ * 
+ * 
+ */
