72750545a242f1c1dc3b2e948cd027245f497de6
gkdltjdrms <121652277+gkdltjdrms@users.noreply.github.com>
2023-02-21 12:44:15 +0900

    Add files via upload

---
+++ Oracle/dbtest/src/dbtest/dao/InsertMain.java
@@ -0,0 +1,17 @@
+package dbtest.dao;
+
+public class InsertMain {
+	
+	public InsertMain() {
+		//driver loading
+		
+	}
+
+	public static void main(String[] args) {
+		InsertMain im = new InsertMain();
+		
+		
+
+	}
+
+}
